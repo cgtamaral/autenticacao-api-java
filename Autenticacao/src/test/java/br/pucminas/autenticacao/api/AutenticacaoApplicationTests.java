546c016dafc39f64ad1e5abd89af91884d5f5766
Cleber Gustavo Tomacheski Amaral <1171193@sga.pucminas.br>
2018-09-27 21:03:41 -0300

    Criação de estrutura inicial do projeto

---
+++ Autenticacao/src/test/java/br/pucminas/autenticacao/api/AutenticacaoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.pucminas.autenticacao.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutenticacaoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
