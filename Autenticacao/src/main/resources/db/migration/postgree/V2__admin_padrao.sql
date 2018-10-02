02d87e1758d082410bff2404ad2e84e1016f833d
Cleber Amaral <cleber.amaral@accenture.com>
2018-10-02 14:00:54 -0300

    Reorganização de pacotes

--- Autenticacao/src/main/resources/db/migration/postgree/V2__admin_padrao.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO USER(id, name, email, password, profile, active, CREATIONDATE) 
-VALUES(1, 'Cleber Amaral', 'cgtamaral@gmail.com', '1234', 'ROLE_ADMIN', true,  to_date('2018-10-01','yyyy-MM-dd'));
