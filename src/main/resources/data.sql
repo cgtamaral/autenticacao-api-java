eb4d00f1e4430ab3963350268c4f538b360a93b1
Cleber Amaral <cleber.amaral@accenture.com>
2018-10-03 01:09:32 -0300

    Configuração profiles DEV, HOM e PRD

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuario(id, name, email, password, profile, active, creationdate) 
+VALUES(1, 'Cleber Amaral', 'cgtamaral@gmail.com', '1234', 'ROLE_ADMIN', true,  to_date('2018-10-01','yyyy-MM-dd'));
